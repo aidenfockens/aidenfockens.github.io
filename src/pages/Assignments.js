936e41101fafee471132230b269b5c3331cf0ac5
aidenfockens <afockens@bu.edu>
2024-12-05 09:57:34 -0500

    added new stuff

--- src/pages/Assignments.js
+++ src/pages/Assignments.js
@@ -163,6 +163,22 @@ function Assignments() {
       </RowContainer>
 
 
+      <RowContainer>
+        <LeftContainer>
+          <AssignmentNameText>Assignment 10</AssignmentNameText>
+          <InfoText>
+            Simple google search algorithm
+          </InfoText>
+          <video width="400" controls>
+            <source src={`${process.env.PUBLIC_URL}/images/assign10.mp4`} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </LeftContainer>
+        <RightContainer>
+            <GreenButton href= "https://github.com/aidenfockens/afockens-assignment-9">Github Link</GreenButton>
+        </RightContainer>
+      </RowContainer>
+
     </PageContainer>
   );
 }
